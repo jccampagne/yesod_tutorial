c8ca5d52be2b26978a202c49550f515e67131427
Jean-Charles Campagne <jc.campagne@gmail.com>
2015-06-22 19:59:21 +0100

    First example

---
+++ basic/Main.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
+
+import Yesod
+import Data.Time.Clock
+import Data.Text
+
+data Master = Master
+
+mkYesod "Master" [parseRoutes|
+/ HomeR GET
+|]
+
+instance Yesod Master
+
+getHomeR :: HandlerT Master IO Html
+getHomeR = defaultLayout $ do
+    [whamlet|
+        <h1>Welcome to Yesod
+        <p>
+            This is the homepage
+    |]
+
+main = warp 3000 $ Master
